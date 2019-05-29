cb6dc1706e5c604c6fe5bfe10d86012bef581c5e
Colton Lacey and Justin Kerntz <justin@>
2019-05-29 13:44:49 -0700

    add zellers rule function

--- src/weekdays.js
+++ src/weekdays.js
@@ -4,17 +4,27 @@ export function Weekdays(date) {
   let k = parseInt(dateArray[2]);
   let d = 0;
   let c = 0;
+  let f = 0;
 
   findDay(dateArray);
 
   function findDay(dateArray){
-    let year = dateArray[0].slice(0, 0);
-
-
-
-    console.log(year);
+    c = parseInt(dateArray[0].slice(0,2));
+    d = parseInt(dateArray[0].slice(2,4));
+    if (m == 1){
+      d = (parseInt(dateArray[0].slice(2,4)))-1;
+      m = 11;
+    }else if (m == 2){
+      d = (parseInt(dateArray[0].slice(2,4)))-1;
+      m = 12;
+    }else{m-=2}
   }
+  console.log(d);
+  console.log(c);
+  console.log(m);
 
-  // f = k + [(13*m-1)/5] + D + [D/4] + [C/4] - 2*C
+  f = k + Math.floor(((13*m-1)/5)) + d + Math.floor((d/4)) + Math.floor((c/4)) - 2*c;
+
+  console.log(f);
   return dateArray;
 }
